81dd1a6894459bca8464fe6ef4dd2b611123ce8e
kxrthika <kirthikaakula@gmail.com>
2026-03-19 17:00:44 +0300

    Add files via upload

---
+++ anumal.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class animal(ABC):
+    def move(self):
+        pass
+class human(animal):
+    def move(self):
+        print("I can walk")
+class snake(animal):
+    def move(self):
+        print("I can crawl")
+class dog (animal):
+    def move(self):
+        print("I can bark")
+class lion(animal):
+    def move(self):
+        print("I can roar")
+R = human()
+R.move()
+K = snake()
+K.move()
+I = dog()
+I.move()
+J = lion()
+J.move()

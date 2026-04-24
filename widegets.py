0445216ae626d3cdde99fbff35eccb80cb807dc6
kxrthika <kirthikaakula@gmail.com>
2026-04-24 17:12:31 +0300

    Add files via upload

---
+++ widegets.py
@@ -0,0 +1,25 @@
+from tkinter import*
+from datetime import date
+root = Tk()
+root.title('getting started with widgets')
+root.geometry('400x300')
+lbl = Label(text="hey there!", fg = "white", bg = "blue", height = 1, width = 300)
+name_lbl = Label (text = "Full Name", bg ="#3895D3")
+name_entry = Entry()
+def display ():
+    name = name_entry.get()
+    global message 
+    message = "welcome to the application !\nToday's date is :"
+    greet = "hello"+name+"\n"
+    text_box.insert(END, greet)
+    text_box.insert(END, message)
+    text_box.insert(END, date.today())
+text_box = Text(height=3)
+btn = Button (text = "begin", command = display, height=1, bg="#1261A0", fg= 'white')
+lbl.pack()
+name_lbl.pack()
+name_entry.pack()
+btn.pack()
+text_box.pack()
+
+root.mainloop()
